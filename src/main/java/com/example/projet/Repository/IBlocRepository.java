9aebe367a76ae4d5ee4bfae7dcd795daf91d7628
hajer <hamrouni.hajer@esprit.tn>
2025-03-21 18:24:24 +0100

    service anvance

--- src/main/java/com/example/projet/Repository/IBlocRepository.java
+++ src/main/java/com/example/projet/Repository/IBlocRepository.java
@@ -5,5 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IBlocRepository extends JpaRepository<Bloc,Long> {
-}
+public interface IChambreRepository extends JpaRepository<Chambre,Long> {
+    @Query("SELECT c FROM Chambre c WHERE c.bloc.foyer.universite.nomUniversite = :nomUniversite AND c.typeC = :type AND c.reservations IS EMPTY")
+    List<Chambre> findChambresNonReservees(String nomUniversite, TypeChambre type);
+
+    @Query("SELECT c FROM Chambre c WHERE c.bloc.idBloc = :idBloc AND c.typeC = :typeC")
+    List<Chambre> findChambresParBlocEtTypeJPQL(long idBloc, TypeChambre typeC);
+
+    List<Chambre> findByBlocIdBlocAndTypeC(long idBloc, TypeChambre typeC);
+
+    List<Chambre> findByNumeroChambreIn(List<Long> numChambres);
