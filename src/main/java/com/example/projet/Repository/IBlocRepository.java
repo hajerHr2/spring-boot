ec6f1daaefaa5a6ed140412989448b93f087a4a7
hajer <hamrouni.hajer@esprit.tn>
2025-04-25 10:34:14 +0100

    final pusHSSS

--- src/main/java/com/example/projet/Repository/IBlocRepository.java
+++ src/main/java/com/example/projet/Repository/IBlocRepository.java
@@ -1,17 +1,2 @@
-package com.example.projet.Repository;
-
-import com.example.projet.Entity.Bloc;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IChambreRepository extends JpaRepository<Chambre,Long> {
-    @Query("SELECT c FROM Chambre c WHERE c.bloc.foyer.universite.nomUniversite = :nomUniversite AND c.typeC = :type AND c.reservations IS EMPTY")
-    List<Chambre> findChambresNonReservees(String nomUniversite, TypeChambre type);
-
-    @Query("SELECT c FROM Chambre c WHERE c.bloc.idBloc = :idBloc AND c.typeC = :typeC")
-    List<Chambre> findChambresParBlocEtTypeJPQL(long idBloc, TypeChambre typeC);
-
-    List<Chambre> findByBlocIdBlocAndTypeC(long idBloc, TypeChambre typeC);
-
-    List<Chambre> findByNumeroChambreIn(List<Long> numChambres);
+package com.example.projet.Repository;public interface IBlocRepository {
+}
--- src/main/java/com/example/projet/Repository/IChambreRepository.java
+++ src/main/java/com/example/projet/Repository/IChambreRepository.java
@@ -1,21 +1,2 @@
-package com.example.projet.Repository;
-
-import com.example.projet.Entity.Chambre;
-import com.example.projet.Entity.TypeChambre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface IChambreRepository extends JpaRepository<Chambre,Long> {
-    @Query("SELECT c FROM Chambre c WHERE c.bloc.foyer.universite.nomUniversite = :nomUniversite AND c.typeC = :type AND c.reservations IS EMPTY")
-    List<Chambre> findChambresNonReservees(String nomUniversite, TypeChambre type);
-
-    @Query("SELECT c FROM Chambre c WHERE c.bloc.idBloc = :idBloc AND c.typeC = :typeC")
-    List<Chambre> findChambresParBlocEtTypeJPQL(long idBloc, TypeChambre typeC);
-
-    List<Chambre> findByBlocIdBlocAndTypeC(long idBloc, TypeChambre typeC);
-
+package com.example.projet.Repository;public interface IChambreRepository {
 }
